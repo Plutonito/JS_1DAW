9fa6d43268397a8d6ccb9008db2f7c7462ed5820
Plutonito <emilio.jpr@gmail.com>
2025-03-19 21:48:25 +0000

    Delete Ejercicio_1-2_Introduccion.js

--- Ejercicio_1-2_Introduccion.js
+++
@@ -1,7 +0,0 @@
-/*
-* cambiamos el document.write por console.log, para que 
-* imprima por pantalla de la consola la información
-*/
-console.log('Emilio');
-console.log('<br>');
-console.log('XX');

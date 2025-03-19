bce964b139453f27c3b2772130a8b6fc06f54165
Plutonito <emilio.jpr@gmail.com>
2025-03-19 21:47:05 +0000

    Actualización

---
+++ Ejercicio_01-2_Introduccion.js
@@ -0,0 +1,7 @@
+/*
+* cambiamos el document.write por console.log, para que 
+* imprima por pantalla de la consola la información
+*/
+console.log('Emilio');
+console.log('<br>');
+console.log('XX');
---
+++ Ejercicio_02-1_Variables.js
@@ -0,0 +1,10 @@
+/* 
+*Definimos las variables pedidas
+*/
+let _nombre="Pedro";
+let $salario=4000;
+
+/*Sacamos por pantalla ambas varibles
+*/
+console.log(_nombre);
+console.log($salario);
